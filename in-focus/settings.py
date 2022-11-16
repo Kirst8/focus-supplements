5c10f3bb86cd7f91f89c53f266f40fef89d3d193
Kirst8 <kirst8.brooker@gmail.com>
2022-11-16 12:56:56 +0000

    update to requirements.txt

--- in-focus/settings.py
+++ in-focus/settings.py
@@ -231,7 +231,7 @@ SOCIAL_AUTH_FACEBOOK_SECRET = os.getenv('SOCIAL_AUTH_FACEBOOK_SECRET', '')
 if "USE_AWS" in os.environ:
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'infocus'
+    AWS_STORAGE_BUCKET_NAME = 'focus-supp'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
