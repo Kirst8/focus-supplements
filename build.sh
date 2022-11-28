85bdc2c3fa0459cf5b2b65954a10e4d094468137
Kirst8 <kirst8.brooker@gmail.com>
2022-11-28 12:01:22 +0000

    deployment change

--- build.sh
+++ build.sh
@@ -1,7 +1,1 @@
-
- set -o errexit
- pip install -r requirements.txt
- python manage.py collectstatic --noinput
- python manage.py makemigrations && python manage.py migrate
-
 web: gunicorn in-focus.wsgi:application
