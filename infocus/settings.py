f5ee226c461bb96efc9f2e614ac7bab894409a15
Kirst8 <kirst8.brooker@gmail.com>
2022-12-03 06:26:24 +0000

    change in seo

--- infocus/settings.py
+++ infocus/settings.py
@@ -22,13 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY',"")
+SECRET_KEY = os.environ.get('SECRET_KEY', "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if "IN_DEVELOPMENT" in os.environ:
     DEBUG = True
 else:
     DEBUG = False
+
 
 
 ALLOWED_HOSTS =["https://in-focus.onrender.com", "localhost"]
