24a114c8566b51b43e9e17c44f61a02240a7eb75
Kirst8 <kirst8.brooker@gmail.com>
2023-01-10 13:38:54 +0000

    env variables changed

--- infocus/settings.py
+++ infocus/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if "DEVELOPMENT" in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
 
 
 
