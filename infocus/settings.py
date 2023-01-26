48eaebdc52b0a41912d6774bbe489ea106540dff
Kirst8 <kirst8.brooker@gmail.com>
2023-01-26 08:49:13 +0000

    env.py added

--- infocus/settings.py
+++ infocus/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-if os.path.exists("env.py"):
-    import env
+from dotenv import load_dotenv
+load_dotenv()
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
